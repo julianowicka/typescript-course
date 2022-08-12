bfdbaeb7f7934082a197a657f0f6262b72e7b489
Julia Nowicka <julia.nowickaa@gmail.com>
2022-08-12 11:57:27 +0200

    Use default props in UserList component

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-interface IUserProps {
-    name: string;
-    email: string;
-    roles?: string[];
-}
-
-const User = ({ roles = [], ...props }: IUserProps) => {
-    return (
-        <li>
-            {props.name} ({props.email}) [{roles.map((role: string) => role)}]
-        </li>
-    );
+const defaultProps = {
+    active: false,
 };
+
+type UserProps = {
+    name: string;
+    active?: boolean;
+} & typeof defaultProps;
+
+const User = ({ name, active }: UserProps) => {
+    return <li className={`${active ? 'user--is-active' : ''}`}>{name}</li>;
+};
+
+User.defaultProps = defaultProps;
+
+type User2Props = typeof User2.defaultProps & {
+    name: string;
+    active: boolean;
+};
+
+class User2 extends Component<User2Props> {
+    static defaultProps = { active: false };
+
+    render() {
+        return <li className={`${this.props.active ? 'user--is-active' : ''}`}>{this.props.name}</li>;
+    }
+}
 
 const UserList = () => {
     return (
         <ul>
-            <User name={'Julia'} email={'julia.nowickaa@gmail.com'} roles={['admin']} />
+            <User name={'Julia'} />
         </ul>
     );
 };
